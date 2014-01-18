0834249a751c4cb6e48a348411218e7a6b5ae9e8
HowMuchi <HowMuchi2014@gmail.com>
2014-01-19 00:03:58 +0800

    fixed

--- delete_act.php
+++ delete_act.php
@@ -17,13 +17,17 @@ $request = $_REQUEST['action'];
 $u_id = $_REQUEST['u_id'];
   if($request==1){			//delete activity
     $check = "select h_id from 5_activity where a_id='$a_id' and h_id='$u_id'";
-//    $result = mysql_query($check);
-
+    $result = mysql_query($check);
+    if($result->rowCount()>0){
     $sql= "delete from 5_activity where a_id='$a_id'";
     $sql2 = "delete from 5_follow where a_id='$a_id'";
     mysql_query($sql);
     mysql_query($sql2);
     echo "Activity deleted";
+    }
+    else{
+    echo "You can't delete this act";
+    }
   }
   else if($request==2){			//cancel attending activity
     $sql3 = "delete from 5_follow where a_id='$a_id' AND f_id='$u_id'";
