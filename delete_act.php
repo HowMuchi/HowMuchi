6c2c4a4611c023997d2db9d62d04b4b1e6bbc25f
YOOO <run2round@hotmail.com.tw>
2014-01-19 00:06:19 +0800

    get_host

--- delete_act.php
+++ delete_act.php
@@ -11,20 +11,20 @@ $db_name = 'nckucampus';
 $conn = mysql_connect($db_host, $db_user, $db_pwd) or die('Error with MySQL connection');
 mysql_query("SET NAMES utf8;", $conn);
 mysql_select_db($db_name)or die(mysql_error());
-
 $a_id = $_REQUEST['a_id'];
 $request = $_REQUEST['action'];
 $u_id = $_REQUEST['u_id']
+  /*
   if($request==1){			//delete activity
     $sql= "delete from 5_activity where a_id='$a_id'";
     $sql2 = "delete from 5_follow where a_id='$a_id'";
-    mysql_query($sql);
-    mysql_query($sql2);
+    mysql_query($conn,$sql)or die(mysql_error());
+    mysql_query($conn,$sql2)or die(mysql_error());
     echo "Activity deleted";
   }
   else if($request==2){			//cancel attending activity
-    $sql3 = "delete from 5_activity where a_id='$a_id' AND f_id='$u_id'";
-    mysql_query($sql3);
-  }
-
+   */
+    $sql3 = "dele/te from 5_activity where a_id='$a_id' AND f_id='$u_id'";
+    //mysql_query($sql3);
+  //}
 ?>
